1a80248e72a4c87874ab87275d8ceceece82a708
felixdasilva <76109537+felixdasilva@users.noreply.github.com>
2020-12-27 20:07:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ def main():
       st.subheader("Consistency")
       st.text("Which two columns should be consistent with each other?")
       Column1 = st.selectbox('Select column 1', df.columns)
-      Column2 = st.selectbox('Select column 2', df.columns)
-      Consistency = Column1.corr(Column2)
-      st.text(Consistency)
+      If Column2 = st.selectbox('Select column 2', df.columns):
+        Consistency = Column1.corr(Column2)
+        st.text(Consistency)
 #the page
 
 if __name__ == '__main__':
