3e1a306bfd14ea65b21eeb0d0bb5ccd642814165
felixdasilva <76109537+felixdasilva@users.noreply.github.com>
2021-04-05 22:36:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -174,8 +174,9 @@ def main():
     st.text(UniquenessScore)
     st.subheader('Column Uniqueness Score')
     st.text(Columns_Selected)
-    for column in df.columns(Columns_Selected):
-        Column_Unique = len(df[Columns_Selected])-len(df[Columns_Selected].drop_duplicates())
+    for column in df[Columns_Selected]):
+        #Column_Unique = len(df[Columns_Selected])-len(df[Columns_Selected].drop_duplicates())
+        Column_Unique = len(df.drop_duplicates())
         st.write('The # of duplicates in this column are:',Column_Unique)
         column_unique_score = ((TotalRows-Column_Unique)/TotalRows)*100
         st.write('The column unique score is:', column_unique_score)
