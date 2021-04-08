59ec60173005361b84e9c9285514d33ad71045e2
felixdasilva <76109537+felixdasilva@users.noreply.github.com>
2021-04-08 19:03:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,11 +31,9 @@ with st.sidebar.beta_expander("Interpretability", expanded=False):
     st.text("10 = Complete understanding")
     if Interpretability == 0:
         Int_Descr = "This measure is not applicable"
-    elif:
-        if Interpretability <5:
+    elif Interpretability <5:
             Int_Descr = "You will likely need someone to explain this data to you"
-        elif:
-            if Interpretability < 8:
+        elif Interpretability < 8:
                 Int_Descr = "You can understand it with some documentation"
             else:
                 Int_Descr = "The data is self explanatory"
