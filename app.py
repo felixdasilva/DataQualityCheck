3cf2732282356b35bf3aed25ae0db4b01749704e
felixdasilva <76109537+felixdasilva@users.noreply.github.com>
2021-04-08 19:59:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -126,8 +126,8 @@ Subj_Score = (Interpretability+Believability+Objectivity+Scarcity+Multifunctiona
 
 #Sujbective Quality Display
 
-st.subheader("Subjective Data Quality")
-st.write("The Overall Subjective Data Quality Score is: ", Subj_Score)
+st.header("Subjective Data Quality")
+st.subheader("The Overall Subjective Data Quality Score is: ", Subj_Score)
 st.write("Subjective Data Quality measures what the users believe to be true. It depends on the user’s prior experience with the data and the task which the data need to solve.")
 st.write("Different users looking at the same data set can come to different conclusions on the quality of the data. These are user input solicited scores.")
 
@@ -140,7 +140,7 @@ st.table(dfsubjective)
 #Objective Sidebar
 
 
-st.sidebar.subheader("Objective Data Quality")
+st.sidebar.header("Objective Data Quality")
 
 #Importing and reading the file
 st.write('Upload your file to calculate your scores.')
