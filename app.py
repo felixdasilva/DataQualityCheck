1a6efde098c5f9492e8d4efe1cbbef8774cbcd3a
felixdasilva <76109537+felixdasilva@users.noreply.github.com>
2021-04-08 20:46:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ def main():
     st.subheader("Timeliness")
     st.write("Time between the last refresh date to the next refresh date. The 'freshness' of the data.")
     st.write("The Timeliness Score is : ", Timeliness)
-    dftimeliness = pd.DataFrame(np.array([LastRefresh, date.today(), NextRefresh]],  columns=['Last Refresh Date','Report Generated Date','Next Refresh Date'])
+    dftimeliness = pd.DataFrame(np.array([[LastRefresh, date.today(), NextRefresh]]),  columns=['Last Refresh Date','Report Generated Date','Next Refresh Date'])
     st.write(dftimeliness)
     
     
