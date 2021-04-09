a0545a1be60457e1d655afa9e2d0cc5a1bc72122
felixdasilva <76109537+felixdasilva@users.noreply.github.com>
2021-04-09 08:30:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -292,8 +292,8 @@ def main():
     
     st.subheader("Accuracy")
     st.write("The data corresponds to reality and is free from bias and material errors.")
-    UpperViolation = (df.accuracy_selected > Upper_Bound).sum()
-    LowerViolation = (df.accuracy_selected < Lower_Bound).sum()
+    UpperViolation = len(df[(df[accuracy_selected]>Upper_Bound))
+    LowerViolation = len(df[(df[accuracy_selected]<Lower_Bound))
     AccuracyViolation = LowerViolation + UpperViolation
     dfaccuracy = pd.DataFrame(np.array([[accuracy_selected, LowerViolation, UpperViolation]]), columns=["Accoracy Proxy", "Lower Bound Violation", "Upper Bound Violation"])
     st.write(dfaccuracy)
