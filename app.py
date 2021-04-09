1dde0c09bc35874a22188ad651bbb252224ffc00
felixdasilva <76109537+felixdasilva@users.noreply.github.com>
2021-04-09 08:20:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ with st.sidebar.beta_expander("4. Apply Validation Rules", expanded=False):
 #Step 5: Specify lower and upper bound
 with st.sidebar.beta_expander("5. Specify Upper / Lower bounds", expanded=False):
     st.write("Select one of the following number columns and define the lower and upper bounds to use as a proxy for accuracy.")
-    accuracy_selected = 1
+    accuracy_selected = 'nothing'
     accuracy_selected = st.selectbox('Select a column', df.columns)
     Lower_Bound = st.number_input('Enter lower bound')
     Upper_Bound = st.number_input('Enter upper bound')
